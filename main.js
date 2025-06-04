1f6c96e8a84fbb578c712b0b4854424302480384
modtanoii <modnoiii@gmail.com>
2025-06-04 16:07:01 +0700

    edit some code

--- main.js
+++ main.js
@@ -1,97 +1,158 @@
-const form = document.getElementById('ticket-form');
+const form = document.getElementById('ticket-form')
 
-const dropArea = document.getElementById('drop-area');
-const fileInput = document.getElementById('file-input');
-const uploadedImage = document.getElementById('uploaded-image');
-const messageAction = document.getElementById('message-action');
-const fileAction = document.getElementById('file-action');
-const removeImage = document.getElementById('remove-image');
-const changeImage = document.getElementById('change-image');
-const uploadHint = document.getElementById('upload-hint');
+const dropArea = document.getElementById('drop-area')
+const fileInput = document.getElementById('file-input')
+const uploadedImage = document.getElementById('uploaded-image')
+const messageAction = document.getElementById('message-action')
+const fileActions = document.getElementById('file-actions')
+const removeImage = document.getElementById('remove-image')
+const changeImage = document.getElementById('change-image')
+const uploadHint = document.getElementById('upload-hint')
 
-const textInputs = document.querySelectorAll('.required');
+const textInputs = document.querySelectorAll('.required')
 
 const formData = {
-    image:'',
+    image: '',
     name: '',
     email: '',
     githubUsername: ''
 }
 
 function validateTextInputs() {
-    let isValid = true;
+    let isValid = true
     
     textInputs.forEach(input => {
         const hint = input.nextElementSibling
 
         if (input.value.trim() === '') {
-            input.classList.add('error');
-            hint.classList.add('error');
+            input.classList.add('error')
+            hint.classList.add('error')
             isValid = false;
         } else {
-            input.classList.remove('error');
-            hint.classList.remove('error');
+            input.classList.remove('error')
+            hint.classList.remove('error')
         }
     })
 
-    return isValid;
+    return isValid
 }
 
 function validateFile(input, hint) {
-    const file = input.files[0];
-    let isValid = true;
+    const file = input.files[0]
+    let isValid = true
 
     if (!file) {
-        hint.classList.add('error');
+        hint.classList.add('error')
+        //svg icon
+        hint.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">< stroke="#f57261" stroke-linecap="round" stroke-linejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"/><path fill="#f57621" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/><path stroke="#f57621" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"></path></svg> Please upload an image.';
+        isValid = false
+    } else {
+        const validTypes = ['image/jpeg', 'image/png']
+        const maxSize = 500 * 1024
+
+        if (!validTypes.includes(file.type)) {
+            hint.classList.add('error')
+            hint.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#f57621" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/><path stroke="#f57621" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"></path></svg> Please upload a valid image (JPEG or PNG).';
+            input.value = ''
+            isValid = false
+        } else if (file.size > maxSize) {
+            hint.classList.add('error')
+            hint.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#f57621" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/><path stroke="#f57621" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"></path></svg> Image size should be less than 500KB.';
+            input.value = ''
+            isValid = false
+        } else {
+            hint.classList.remove('error')
+            hint.innerHTML = '<img src="assets/images/icon-info.svg" alt=""> Upload your photo (JPEG or PNG, max 500KB)';
+            displayUploadedImage(file)
+        }
     }
+
+    return isValid;
+
+}
+
+function displayUploadedImage(file) {
+    const reader = new FileReader()
+
+    reader.onload = e => {
+        uploadedImage.src = e.target.result
+        fileActions.classList.add('show')
+        messageAction.classList.add('hide')
+    }
+
+    reader.readAsDataURL(file)
+}
+
+function resetUpload() {
+    const defaultUploadIcon = 'assets/images/icon-upload.svg'
+
+    fileInput.value = ''
+    uploadedImage.src = defaultUploadIcon
+    messageAction.classList.remove('hide')
+    fileActions.classList.remove('show')
+    uploadHint.classList.remove('error')
+    uploadHint.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">< stroke="#f57261" stroke-linecap="round" stroke-linejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"/><path fill="#f57621" d="M8.004 10.462V7.596ZM8 5.57v-.042Z"/><path stroke="#f57621" stroke-linecap="round" stroke-linejoin="round" d="M8.004 10.462V7.596M8 5.569v-.042"></path></svg> Upload your photo (JPEG or PNG, max 500KB)';
+}
+
+function storeAndDisplayFormData() {
+    formData.image = uploadedImage.src
+    formData.name = document.getElementById('full-name').value.trim()
+    formData.email = document.getElementById('email').value.trim()
+    formData.githubUsername = document.getElementById('github').value.trim()
+
+    document.getElementById('header-name').textContent = formData.name
+    document.getElementById('display-name').textContent = formData.name
+    document.getElementById('display-email').textContent = formData.email
+    document.getElementById('display-github').textContent = formData.githubUsername
+    document.getElementById('display-image').src = formData.image
 }
 
 dropArea.addEventListener('click', () => {
-    fileInput.click();
+    fileInput.click()
 })
 
 dropArea.addEventListener('dragover', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     return
 })
 
 dropArea.addEventListener('drop', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     
-    const files = e.dataTransfer.files;
+    const files = e.dataTransfer.files
     if (files.length > 0) {
         fileInput.files = files
         validateFile(fileInput, uploadHint)
     }
 })
 
-fileInput.addEventListener('change', (e) => {
+fileInput.addEventListener('change', () => {
     validateFile(fileInput, uploadHint)
 })
 
-removeImage.addEventListener('click', () => {
-    e.preventDefault();
-    e.stopPropagation();
-    resetUpload();
+removeImage.addEventListener('click', (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    resetUpload()
 })
 
-changeImage.addEventListener('click', () => {
-    e.preventDefault();
-    e.stopPropagation();
-    fileInput.click();
+changeImage.addEventListener('click', (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    fileInput.click()
 })
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+    e.preventDefault()
     
-    const isTextValid = validateTextInputs();
-    const isFileValid = validateFile(fileInput, uploadHint);
+    const isTextValid = validateTextInputs()
+    const isFileValid = validateFile(fileInput, uploadHint)
 
     if (isTextValid && isFileValid) {
-        storeAndDisplayFormdata();
+        storeAndDisplayFormData()
 
-        document.getElementById('form-container').classList.add('hide');
-        document.getElementById('diaplay-data').style.display = 'block';
+        document.getElementById('form-content').classList.add('hide')
+        document.getElementById('display-data').style.display = 'block'
     }
 
 })
